b2eec12037d2f9e502f4b45ff7c0bdae9bab4d00
HenryTam123 <u3557967@connect.hku.hk>
2020-08-18 16:07:54 +0800

    Updates

---
+++ precache-manifest.ccdbe65b119c801e571935af4aa163a0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c68e924cf3119b7496bfb4e5258ff4c6",
+    "url": "https://github.com/HenryTam123/music/tree/gh-pages/sounds/index.html"
+  },
+  {
+    "revision": "c011598754a6b6a55762",
+    "url": "https://github.com/HenryTam123/music/tree/gh-pages/sounds/static/css/main.83472d6d.chunk.css"
+  },
+  {
+    "revision": "7912ecaede81997679ea",
+    "url": "https://github.com/HenryTam123/music/tree/gh-pages/sounds/static/js/2.850e13a9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "https://github.com/HenryTam123/music/tree/gh-pages/sounds/static/js/2.850e13a9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c011598754a6b6a55762",
+    "url": "https://github.com/HenryTam123/music/tree/gh-pages/sounds/static/js/main.7ebaa508.chunk.js"
+  },
+  {
+    "revision": "1a1e32c18f441d30a243",
+    "url": "https://github.com/HenryTam123/music/tree/gh-pages/sounds/static/js/runtime-main.862caf5d.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/music/precache-manifest.68dcf0727f73656ccfae118f70244644.js"
+  "https://github.com/HenryTam123/music/tree/gh-pages/sounds/precache-manifest.ccdbe65b119c801e571935af4aa163a0.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/music/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("https://github.com/HenryTam123/music/tree/gh-pages/sounds/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
