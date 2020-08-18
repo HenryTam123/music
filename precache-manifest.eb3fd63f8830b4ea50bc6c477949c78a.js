24f07c1f3f1acb385c9a1d392743eca6c19a9a06
HenryTam123 <u3557967@connect.hku.hk>
2020-08-18 16:13:36 +0800

    Updates

---
+++ precache-manifest.eb3fd63f8830b4ea50bc6c477949c78a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c2e02cda72c795bbc2ce37a7872a6bb9",
+    "url": "/music/index.html"
+  },
+  {
+    "revision": "5832c30851d4e3106bdf",
+    "url": "/music/static/css/main.83472d6d.chunk.css"
+  },
+  {
+    "revision": "7912ecaede81997679ea",
+    "url": "/music/static/js/2.850e13a9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/music/static/js/2.850e13a9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5832c30851d4e3106bdf",
+    "url": "/music/static/js/main.2a8ea046.chunk.js"
+  },
+  {
+    "revision": "0f6dfe125efd8c4a4ed7",
+    "url": "/music/static/js/runtime-main.145e4bdc.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "https://github.com/HenryTam123/music/tree/master/public/precache-manifest.3da3db72500d1800576950bc62560287.js"
+  "/music/precache-manifest.eb3fd63f8830b4ea50bc6c477949c78a.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("https://github.com/HenryTam123/music/tree/master/public/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/music/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
